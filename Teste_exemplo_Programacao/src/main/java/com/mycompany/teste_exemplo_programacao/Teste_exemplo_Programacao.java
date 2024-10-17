00cde4992aa5a6095f2a8840f64d229afe907a9c
Franquilei <franquilei.oliveira@aluno.senai.br>
2024-10-17 20:38:11 -0300

    Add files via upload

---
+++ Teste_exemplo_Programacao/src/main/java/com/mycompany/teste_exemplo_programacao/Teste_exemplo_Programacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste_exemplo_programacao;
+
+/**
+ *
+ * @author f.oliveira
+ */
+public class Teste_exemplo_Programacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
